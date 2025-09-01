7d2dbbc9dc7bf171b9385d997853ea79797a25f0
anshika-pathak005 <anshikapathak9026@gmail.com>
2025-09-01 22:30:38 +0530

    Add initial project files

---
+++ index.js
@@ -0,0 +1,21 @@
+const display = document.querySelector(".result_display")
+
+function appendToDisplay(input){
+    display.value += input
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+function cutLast(){ 
+    display.value = display.value.slice(0, -1); 
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value)
+    }
+    catch{
+        display.value = "Error";
+    }
+}
